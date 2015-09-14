58cfdb8ac9bac068386b2f6d068244b5329a69d5
igotyooo <overmars29@gmail.com>
2015-09-14 16:27:16 +0900

    Add box merging after proposal.

--- appAttNetClsAgn/PropObj.m
+++ appAttNetClsAgn/PropObj.m
@@ -5,22 +5,29 @@ classdef PropObj < handle
         stride;
         patchSide;
         scales;
-        setting;
+        settingMain;
+        settingPost;
     end
     methods( Access = public )
-        function this = PropObj( db, attNet, setting )
+        function this = PropObj( db, attNet, settingMain, settingPost )
             this.db = db;
             this.attNet = attNet;
-            this.setting.numScaling = 24;
-            this.setting.dilate = 1 / 4;
-            this.setting.posIntOverRegnMoreThan = 1 / 3;
-            this.setting = setChanges...
-                ( this.setting, setting, upper( mfilename ) );
+            this.settingMain.numScaling = 24;
+            this.settingMain.dilate = 1 / 4;
+            this.settingMain.posIntOverRegnMoreThan = 1 / 3;
+            this.settingPost.mergingOverlap = 0.7;
+            this.settingPost.mergingType = 'OV'; 'NMS';
+            this.settingPost.mergingMethod = 'WAVG'; 'MAX';
+            this.settingPost.minimumNumSupportBox = 0;
+            this.settingMain = setChanges...
+                ( this.settingMain, settingMain, upper( mfilename ) );
+            this.settingPost = setChanges...
+                ( this.settingPost, settingPost, upper( mfilename ) );
         end
         function init( this, gpus )
             % Set parameters.
-            numScaling = this.setting.numScaling;
-            posIntOverRegnMoreThan = this.setting.posIntOverRegnMoreThan;
+            numScaling = this.settingMain.numScaling;
+            posIntOverRegnMoreThan = this.settingMain.posIntOverRegnMoreThan;
             % Fetch net on GPU.
             this.attNet.layers{ end }.type = 'softmax';
             this.attNet = Net.fetchNetOnGpu( this.attNet, gpus );
@@ -95,17 +102,21 @@ classdef PropObj < handle
                 rid2ok = rid2okCls & rid2okDir;
                 rid2outCls = rid2outCls( :, rid2ok );
                 rid2outDir = rid2outDir( :, rid2ok );
-                % rid2scoreCls = rid2scoreCls( rid2ok ) * 2 - sum( rid2outCls, 1 );
                 rid2scoreCls = rid2scoreCls( rid2ok ) - ...
                     sum( rid2outCls( numel( dimCls ) - 1 : end, : ), 1 );
                 rid2scoreDir = rid2scoreDir( rid2ok ) * 2 - sum( rid2outDir, 1 );
                 rid2score = rid2scoreCls + rid2scoreDir;
                 rid2cid = rid2cidCls( rid2ok );
                 rid2tlbr = rid2tlbr( 1 : 4, rid2ok );
+                % Post-processing: merge bounding boxes.
+                if this.settingPost.mergingOverlap ~= 1,
+                    [ rid2tlbr, rid2score, rid2cid ] = ...
+                        this.merge( rid2tlbr, rid2score, rid2cid );
+                end;
             end;
         end
         function [ rid2out, rid2tlbr ] = im2det( this, im )
-            dilate = this.setting.dilate;
+            dilate = this.settingMain.dilate;
             [ r, c, ~ ] = size( im );
             imSize0 = [ r; c; ];
             sid2size = round( bsxfun( @times, this.scales, imSize0 ) );
@@ -127,11 +138,45 @@ classdef PropObj < handle
             if size( rid2out, 2 ) ~= size( rid2tlbr, 2 ),
                 error( 'Inconsistent number of regions.\n' ); end;
         end
+        function [ rid2tlbr, rid2score, rid2cid ] = ...
+                merge( this, rid2tlbr, rid2score, rid2cid )
+            mergingOverlap = this.settingPost.mergingOverlap;
+            mergingType = this.settingPost.mergingType;
+            mergingMethod = this.settingPost.mergingMethod;
+            minNumSuppBox = this.settingPost.minimumNumSupportBox;
+            cids = unique( rid2cid );
+            numCls = numel( cids );
+            rid2tlbr_ = cell( numCls, 1 );
+            rid2score_ = cell( numCls, 1 );
+            rid2cid_ = cell( numCls, 1 );
+            for cidx = 1 : numCls,
+                cid = cids( cidx );
+                rid2ok = rid2cid == cid;
+                switch mergingType,
+                    case 'NMS',
+                        [ rid2tlbr_{ cidx }, rid2score_{ cidx } ] = nms( ...
+                            [ rid2tlbr( :, rid2ok ); rid2score( rid2ok ); ]', ...
+                            mergingOverlap, minNumSuppBox, mergingMethod );
+                        rid2tlbr_{ cidx } = rid2tlbr_{ cidx }';
+                    case 'OV',
+                        [ rid2tlbr_{ cidx }, rid2score_{ cidx } ] = ov( ...
+                            rid2tlbr( :, rid2ok ), rid2score( rid2ok ), ...
+                            mergingOverlap, minNumSuppBox, mergingMethod );
+                end
+                rid2cid_{ cidx } = cid * ones( size( rid2score_{ cidx } ) );
+            end;
+            rid2tlbr = cat( 2, rid2tlbr_{ : } );
+            rid2score = cat( 1, rid2score_{ : } );
+            rid2cid = cat( 1, rid2cid_{ : } );
+            [ rid2score, idx ] = sort( rid2score, 'descend' );
+            rid2tlbr = rid2tlbr( :, idx );
+            rid2cid = rid2cid( idx );
+        end
         % Functions for identification.
         function name = getName( this )
             name = sprintf( ...
                 'PROP_%s_OF_%s', ...
-                this.setting.changes, ...
+                this.settingMain.changes, ...
                 this.attNet.name );
             name( strfind( name, '__' ) ) = '';
             if name( end ) == '_', name( end ) = ''; end;
