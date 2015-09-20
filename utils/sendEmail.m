71f35fe27e7c3c214f524dba89c8d50e82e29078
igotyooo <overmars29@gmail.com>
2015-09-20 16:08:03 +0900

    Adds exception handling of sending email.

--- utils/sendEmail.m
+++ utils/sendEmail.m
@@ -1,17 +1,22 @@
 function sendEmail( adrssFrom, pwdFrom, adrssTo, title, mssg, attchFpaths )
-    % Gmail server supported only.
-    setpref( 'Internet', 'SMTP_Server', 'smtp.gmail.com' );
-    setpref( 'Internet', 'E_mail', adrssFrom );
-    setpref( 'Internet', 'SMTP_Username', adrssFrom );
-    setpref( 'Internet', 'SMTP_Password', pwdFrom );
-    setpref( 'Internet', 'E_mail_Charset', 'SJIS' );
-    props = java.lang.System.getProperties;
-    props.setProperty( 'mail.smtp.auth', 'true' );
-    props.setProperty( 'mail.smtp.socketFactory.class',  'javax.net.ssl.SSLSocketFactory' );
-    props.setProperty( 'mail.smtp.socketFactory.port', '465' );
-    if isempty( attchFpaths )
-        sendmail( adrssTo, title, mssg );
-    else
-        sendmail( adrssTo, title, mssg, attchFpaths );
-    end
+    
+    try
+        % Gmail server supported only.
+        setpref( 'Internet', 'SMTP_Server', 'smtp.gmail.com' );
+        setpref( 'Internet', 'E_mail', adrssFrom );
+        setpref( 'Internet', 'SMTP_Username', adrssFrom );
+        setpref( 'Internet', 'SMTP_Password', pwdFrom );
+        setpref( 'Internet', 'E_mail_Charset', 'SJIS' );
+        props = java.lang.System.getProperties;
+        props.setProperty( 'mail.smtp.auth', 'true' );
+        props.setProperty( 'mail.smtp.socketFactory.class',  'javax.net.ssl.SSLSocketFactory' );
+        props.setProperty( 'mail.smtp.socketFactory.port', '465' );
+        if isempty( attchFpaths )
+            sendmail( adrssTo, title, mssg );
+        else
+            sendmail( adrssTo, title, mssg, attchFpaths );
+        end
+    catch
+        
+    end;
 end
