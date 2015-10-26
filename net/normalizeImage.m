cd42593fc8b1e931dd2565d15e260faebc2adff0
igotyooo <overmars29@gmail.com>
2015-10-27 00:30:42 +0900

    Adds channel-resize with a warning message.

--- net/normalizeImage.m
+++ net/normalizeImage.m
@@ -1,7 +1,9 @@
 function image = normalizeImage( image, averageImage, interpolation )
     [ r, c, ch ] = size( image );
     [ rav, cav, chav ] = size( averageImage );
-    if ch ~= chav, error( 'Inconsistent channels.\n' ); end;
+    if ch ~= chav, warning( 'Inconsistent channels.' ); end;
+    if chav == 1 && ch == 3, image = rgb2gray( image ); end;
+    if chav == 3 && ch == 1, image = cat( 3, image, image, image ); end;
     if ( rav * cav ~= 1 ) && ( r ~= rav || c ~= cav ), ...
             averageImage = imresize( averageImage, [ r, c ], 'method', interpolation ); end;
     image = bsxfun( @minus, image, averageImage );
