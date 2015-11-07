ed07e8b9b69bd7d460d696a498336defda061425
igotyooo <overmars29@gmail.com>
2015-11-08 07:07:39 +0900

    Sigmoid score for weighted average of the boxes.

--- appAttNetCaffe/AttNetCaffe.m
+++ appAttNetCaffe/AttNetCaffe.m
@@ -192,6 +192,7 @@ classdef AttNetCaffe < handle
             end;
             if nargout,
                 % 3. Merge regions.
+                rid2score = 1 ./ ( 1 + exp( -rid2score ) );
                 [ rid2tlbr, rid2score, rid2cid ] = this.merge...
                     ( rid2tlbr, rid2score, rid2cid, this.settingMrg0 );
                 imbnd = [ 1; 1; this.db.iid2size( :, iid ); ];
@@ -223,6 +224,7 @@ classdef AttNetCaffe < handle
             end;
             if nargout,
                 % 3. Merge regions.
+                rid2score = 1 ./ ( 1 + exp( -rid2score ) );
                 [ rid2tlbr, rid2score, rid2cid ] = this.merge...
                     ( rid2tlbr, rid2score, rid2cid, this.settingMrg1 );
                 imbnd = [ 1; 1; this.db.iid2size( :, iid ); ];
@@ -262,6 +264,7 @@ classdef AttNetCaffe < handle
             title( sprintf( 'Detection0, IID%06d', iid ) ); hold off;
             setFigPos( gcf, [ 3, 6, 1, 3 ] ); drawnow;
             % Demo 3: Merge0.
+            rid2score = 1 ./ ( 1 + exp( -rid2score ) );
             [ rid2tlbr, ~, rid2cid ] = this.merge...
                 ( rid2tlbr, rid2score, rid2cid, this.settingMrg0 );
             imbnd = [ 1; 1; this.db.iid2size( :, iid ); ];
@@ -297,6 +300,7 @@ classdef AttNetCaffe < handle
             title( sprintf( 'Detection1, IID%06d', iid ) ); hold off;
             setFigPos( gcf, [ 3, 6, 1, 5 ] ); drawnow;
             % Demo 5: Merge1.
+            rid2score = 1 ./ ( 1 + exp( -rid2score ) );
             [ rid2tlbr, rid2score, rid2cid ] = this.merge...
                 ( rid2tlbr, rid2score, rid2cid, this.settingMrg1 );
             imbnd = [ 1; 1; this.db.iid2size( :, iid ); ];
