fd70684b11ff7f937b439dd7f945b6ce6f10f407
cvpa3 <dgyoo@rcv.kaist.ac.kr>
2016-01-12 21:27:17 +0900

    Clean up.

--- appMpp/appMpp.m
+++ appMpp/appMpp.m
@@ -1,12 +1,12 @@
 %% SET PARAMETERS ONLY.
 clc; close all; fclose all; clear all; 
 addpath( genpath( '..' ) ); init;
+setting.dstDir                          = '/nickel/data_mpp/';
 setting.gpu                             = 1;
 setting.db                              = path.db.voc2007;
-setting.net                             = path.net.caffeRef;
+setting.net                             = path.net.vgg_m;
 setting.neuralRegnDesc.layerId          = 19;
 setting.neuralRegnDesc.scalingCriteria  = 'MIN';
-setting.neuralRegnDesc.scaleId2numPixel = round( 227 * 2 .^ ( 0 : 0.5 : 3 ) );
 setting.neuralRegnDesc.pcaDim           = 128;
 setting.neuralRegnDesc.kernelBeforePca  = 'NONE';
 setting.neuralRegnDesc.normBeforePca    = 'L2';
@@ -14,7 +14,6 @@ setting.neuralRegnDesc.normAfterPca     = 'L2';
 setting.neuralRegnDesc.regionFiltering  = '';
 setting.neuralRegnDic.numTargetScale    = +Inf;
 setting.neuralRegnDic.numGaussian       = 256;
-setting.fisher.selectScales             = 1 : numel( setting.neuralRegnDesc.scaleId2numPixel ) - 1;
 setting.fisher.scaleWeightingMethod     = 'L2NORM'; % NONE, ENTROPY.
 setting.fisher.spatialPyramid           = '11';
 setting.svm.kernel                      = 'NONE';
@@ -32,10 +31,12 @@ setting.svm.solver                      = 'SDCA';
 
 %% DO THE JOB.
 reset( gpuDevice( setting.gpu ) );
-db = Db( setting.db, path.dstDir );
+db = Db( setting.db, setting.dstDir );
 db.genDb;
 net = load( setting.net.path );
 net.name = setting.net.name;
+patchSide = getNetProperties( net, setting.neuralRegnDesc.layerId );
+setting.neuralRegnDesc.scaleId2numPixel = round( patchSide * 2 .^ ( 0 : 0.5 : 3 ) );
 neuralRegnDscrber = ...
     NeuralRegnDscrber( db, net, ...
     setting.neuralRegnDesc, ...
@@ -43,6 +44,7 @@ neuralRegnDscrber = ...
 neuralRegnDscrber.init( setting.gpu );
 neuralRegnDscrber.trainDic;
 neuralRegnDscrber.descDb;   % Possibly skipped.
+setting.fisher.selectScales = 1 : numel( setting.neuralRegnDesc.scaleId2numPixel ) - 1;
 fisher = Fisher( neuralRegnDscrber, setting.fisher );
 imDscrber = ImDscrber( db, { fisher }, [  ] );
 imDscrber.descDb;
